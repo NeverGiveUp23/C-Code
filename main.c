1f216a779cf33cac60f8c2d3d83c891d753a9a7e
felix <felixvjr0@gmail.com>
2024-10-13 23:34:53 -0400

    C code

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main(){
+
+    int myNumber = 0;
+    char str[100];
+
+    scanf("%d %s", &myNumber , str);
+
+    printf("You entered: %d and %s", myNumber, str);
+
+    return 0;
+
 }
+
+
