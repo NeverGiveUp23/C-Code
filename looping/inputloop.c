f5f76e2ba4a15b56bb2194dcaf5d0e4225fb8b96
felix <felixvjr0@gmail.com>
2024-10-25 13:59:13 -0400

    Mesing around with for loops

--- looping/inputloop.c
+++ looping/inputloop.c
@@ -3,15 +3,24 @@
 
 int main(void) {
 
-    int n, triangleNumber, number, counter;
+    int n, triangleNumber, number, counter, n2;
 
     // this is a nested for loop
     // the loop outside will run the loop inside n times
     // and will ask me n times to pick a number to triangulate.
+    // this can help if the user has n amount of numbers to calculate and triangulate
 
-    for (counter = 1; counter <= 2 ; ++counter) {
+    printf("How many numbers do you need to Triangulate? ");
+    scanf("%i", &n2);
 
-        printf("What Triangle number do you want to enter? ");
+    for (counter = 1; counter <= n2 ; ++counter) {
+        if (n2 == 1){ // checking if the input from user is equal 1 to get a unique printf
+            printf("What Triangle number do you want to enter? ");
+        } else if(counter < n2){ // if the count is less than the user input number
+            printf("Enter the other Triangle number you want to enter? ");
+        }else if(counter == n2){ // if count equals the user input number.
+            printf("Enter the last Triangle number you want to enter? ");
+        }
         scanf("%i", &number);
 
         triangleNumber = 0;
