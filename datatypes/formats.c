72eca645057e37e0a8d8642a3e0bde29ae104208
felix <felixvjr0@gmail.com>
2024-10-25 01:04:55 -0400

    Adding C format specifiers

---
+++ datatypes/formats.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+
+int main(void){
+
+    // format specifiers are used when displaying variables as output
+    int integerVar = 100;
+    float floatingVar = 333.21f;
+    double doubleVar = 9.4423e+11;
+    char charVar = 'D';
+    _Bool boolVar = 0;
+
+    float floatVar = 3.993423f;
+
+
+    printf("integerVar = %i\n", integerVar);
+    printf("floatingVar = %f\n", floatingVar);
+    printf("doubleVar = %e\n", doubleVar);
+    printf("charVar = %c\n", charVar);
+
+    printf("boolVar = %i\n", boolVar); // can input the specifier as an int for bool due to it being an int under the hood
+
+    printf("floating var and rounding: %.2f\n", floatVar);
+    // output ---> floating var and rounding: 3.99
+    printf("floating var and rounding: %.5f\n", floatVar);
+    // output ---> floating var and rounding: 3.99342
+
+
+    // outcome
+/*
+integerVar = 100
+floatingVar = 333.209991
+doubleVar = 9.442300e+11
+charVar = D
+boolVar = 0
+
+ */
+
+
+    return 0;
+}
